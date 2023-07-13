034fa597609fc8aba3f4935e5394b65468873f42
Bramireza <bramirezag@gmail.com>
2023-07-12 22:13:43 -0500

    fix env

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,56 @@
 import { MainLayout } from "../../layouts";
+import {
+  Link,
+  Box,
+  Grid,
+  Divider,
+  Card,
+  CardContent,
+  Button,
+} from "@mui/material";
 
 const Home = () => {
   return (
     <MainLayout>
-      <>Welcome Home</>
+      <Card sx={{ zIndex: 1, boxShadow: "none", mt: 5, borderRadius: 5 }}>
+        <CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: 2,
+            }}
+          >
+            <Box>
+              <Grid
+                container
+                sx={{ justifyContent: "center", textAlign: "center" }}
+              >
+                <Grid item>
+                  <Button
+                    href="/signin"
+                    variant="contained"
+                    sx={{ mt: 5, mb: 2 }}
+                    fullWidth
+                  >
+                    Inicia Sesion aqui
+                  </Button>
+                  <Divider>o</Divider>
+                  <Button
+                    href="/signup"
+                    variant="contained"
+                    sx={{ mt: 5, mb: 2 }}
+                    fullWidth
+                  >
+                    Registrate aquí
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
     </MainLayout>
   );
 };
